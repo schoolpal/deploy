d123ba655884782cc72302b3f66df089f7522db6
Gary Zhu <dinner3000@hotmail.com>
2017-12-30 22:01:26 +0800

    Refine

--- test/deploy.sh
+++ test/deploy.sh
@@ -155,19 +155,20 @@ echo -n "Generate docker-compose file ... "
 if [ -f ${DOCKER_COMPOSE_FILE} ]; then
     echo -n "already exits, skip. (delete it if you want to re-generate)"
 else
+    if [ ${REFRESH_DATABASE} == "Y" ]; then
+        VOLUME_INITSQL_LINE=" - $(echo "${DOCKER_VOLUME}/${VOLUME_INITSQL}" | sed 's/\//\\\//g'):/docker-entrypoint-initdb.d:ro"
+    else
+        VOLUME_INITSQL_LINE=""
+    fi
     cat ${DOCKER_COMPOSE_FILE_TPL} | \
-    sed 's/_PORT_NGINX_/"${PORT_NGINX}"/g' | \
-    sed 's/_PORT_NGINX_/"${PORT_TOMCAT}"/g' | \
-    sed 's/_PORT_NGINX_/"${PORT_REDIS}"/g' | \
-    sed 's/_PORT_NGINX_/"${PORT_MYSQL}"/g' | \
+    sed 's/_PORT_NGINX_/'${PORT_NGINX}'/g' | \
+    sed 's/_PORT_TOMCAT_/'${PORT_TOMCAT}'/g' | \
+    sed 's/_PORT_REDIS_/'${PORT_REDIS}'/g' | \
+    sed 's/_PORT_MYSQL_/'${PORT_MYSQL}'/g' | \
     sed 's/_VOLUME_LOGS_/'$(echo "${DOCKER_VOLUME}/${VOLUME_LOGS}" | sed 's/\//\\\//g')'/g' | \
     sed 's/_VOLUME_DATA_/'$(echo "${DOCKER_VOLUME}/${VOLUME_DATA}" | sed 's/\//\\\//g')'/g' \
+    sed 's/_VOLUME_INITSQL_/'${VOLUME_INITSQL_LINE}'/g' \
     > ${DOCKER_COMPOSE_FILE}
-    if [ ${REFRESH_DATABASE} == "Y" ]; then
-        sed 's/_VOLUME_INITSQL_/ - '$(echo "${DOCKER_VOLUME}/${VOLUME_INITSQL}" | sed 's/\//\\\//g')':/docker-entrypoint-initdb.d:ro/g' \
-        ${DOCKER_COMPOSE_FILE} \
-        > ${DOCKER_COMPOSE_FILE}
-    fi
     echo "done"
 fi
 
